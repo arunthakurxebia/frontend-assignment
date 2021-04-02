b27ec0e6c0bf3177816cbc495bf531eaef0a21e7
Arun Thakur <arun.thakur@xebia.com>
2021-04-02 17:47:54 +0500

    US#1 header with my bank's logo and the footer

--- peachtree-bank/src/app/app.module.ts
+++ peachtree-bank/src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { FooterComponent } from './footer/footer.component';
+import { LogoComponent } from './logo/logo.component';
+import { SubmitButtonComponent } from './submit-button/submit-button.component';
+import { TransactionItemComponent } from './transaction-item/transaction-item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    LogoComponent,
+    SubmitButtonComponent,
+    TransactionItemComponent
   ],
   imports: [
     BrowserModule
