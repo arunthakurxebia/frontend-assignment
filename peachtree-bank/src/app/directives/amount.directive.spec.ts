b6beeee0f7ef5379e9fc6050c91f2e25a7e96a73
Arun Thakur <arun.thakur@xebia.com>
2021-04-06 16:35:40 +0530

    Validation updated

--- peachtree-bank/src/app/directives/amount.directive.spec.ts
+++ peachtree-bank/src/app/directives/amount.directive.spec.ts
@@ -1,8 +1,10 @@
 import { AmountDirective } from './amount.directive';
-
+let elRefMock = {
+  nativeElement: document.createElement('div')
+};
 describe('AmountDirective', () => {
   it('should create an instance', () => {
-    const directive = new AmountDirective();
+    const directive = new AmountDirective(elRefMock);
     expect(directive).toBeTruthy();
   });
 });
